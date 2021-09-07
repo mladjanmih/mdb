736af8f173c35b93bd87316bffb10002117b4144
mladjanmih <mladjan.mihajlovickg@gmail.com>
2021-09-07 17:23:34 +0200

    Update zadatak1.js

--- zadatak1.js
+++ zadatak1.js
@@ -20,7 +20,20 @@ db.knjizara.find(
   {
       naslov: 1,
       autor: 1,
-      brojSrana: 1,
-      cena: 1
+      brojStrana: 1,
+      cena: 1,
+      _id: 0
   }
+)
+
+//2.
+db.knjizara.find(
+    {
+        godinaIzdanja: { $gte: 2000, $lte: 2005 }
+    },
+    {
+        naslov: 1,
+        godinaIzdanja: 1,
+        _id: 0
+    }
 )
